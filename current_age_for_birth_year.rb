f6b9ecb2045182614ee6a82c633bcdd677a41af5
Blaine R Love <blainelove94@gmail.com>
2020-12-10 17:34:01 +0000

    Automatically backed up by Learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,4 +1,2 @@
-require_relative '../current_age_for_birth_year.rb'
-
-describe "current_age_for_birth_year method" do
-  it "returns the age of a person based on their year of birth" do
+def current_age_for_birth_year(birth_year)
+end
